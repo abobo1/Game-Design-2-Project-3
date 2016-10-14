0d78676e503cf0649c5824a75999a4e79fba6c5c
Steven Cortez <steven_cortez@msn.com>
2016-10-13 21:26:24 -0500

    Added some with the main menu

---
+++ Assets/Scripts/mainMenu.cs
@@ -0,0 +1,47 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class mainMenu : MonoBehaviour {
+    public GameObject[] mainButtons,optionButtons,gameMenu;
+    
+
+    void Start()
+    {
+        DontDestroyOnLoad(gameObject);
+        for (int k = 0; k < mainButtons.Length; k++)
+            mainButtons[k].SetActive(true);
+        for (int k = 0; k < optionButtons.Length; k++)
+            optionButtons[k].SetActive(false);
+        for (int k = 0; k < gameMenu.Length; k++)
+            gameMenu[k].SetActive(false);
+    }
+    public void changeMenu(string name)
+    {
+        for (int k = 0; k < mainButtons.Length; k++)
+            mainButtons[k].SetActive(false);
+        if (name == "Exit")
+            Application.Quit();
+        else if (name == "Options")
+            for (int k = 0; k < optionButtons.Length; k++)
+                optionButtons[k].SetActive(true);
+        else if (name == "Return")
+        {
+            for (int k = 0; k < mainButtons.Length; k++)
+                mainButtons[k].SetActive(true);
+            for (int k = 0; k < optionButtons.Length; k++)
+                optionButtons[k].SetActive(false);
+            for (int k = 0; k < gameMenu.Length; k++)
+                gameMenu[k].SetActive(false);
+        }
+        else if (name == "Continue" || name == "New")
+        {
+            for (int k = 0; k < gameMenu.Length; k++)
+                gameMenu[k].SetActive(true);
+        }
+
+
+
+    }
+
+	
+}
