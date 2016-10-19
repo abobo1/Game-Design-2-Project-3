38015f55a4e0fddb894209ad94da1fc495cc5cc1
kanestapler <kanestapler@gmail.com>
2016-10-19 10:41:27 -0500

    Making quest completed add/remove questMark

--- Assets/Scripts/NPC/NPCTalk.cs
+++ Assets/Scripts/NPC/NPCTalk.cs
@@ -12,24 +12,38 @@ public class NPCTalk : MonoBehaviour {
     private Quests allQuests = new Quests();
 
     private GameObject player;
+    private GameObject questMark;
 
 	// Use this for initialization
 	void Start () {
         player = GameObject.FindGameObjectWithTag("Player");
+        questMark = GameObject.Find("QuestMark");
         questDescription = allQuests.GetQuestDescription(questName);
 	}
+
+    void Update() {
+        //proof of concept
+        if (IsQuestCompleted()) {
+            questMark.SetActive(false);
+        }
+    }
 
     void OnMouseOver() {
         if (Input.GetMouseButtonDown(1)) {
             print("Right click on this object");
             if (Vector3.Distance(this.transform.position, player.transform.position) < talkingRegisterDistance) {
                 print("You are close enough");
-                if (!player.GetComponent<PlayerQuests>().IsQuestCompleted(questName)) {
+                if (!IsQuestCompleted()) {
                     print("Quest is not completed yet");
                 } else {
                     print("Quest is completed");
                 }
             }
         }
+    }
+
+
+    private bool IsQuestCompleted() {
+        return player.GetComponent<PlayerQuests>().IsQuestCompleted(questName);
     }
 }
--- Assets/Scripts/Player/PlayerQuests.cs
+++ Assets/Scripts/Player/PlayerQuests.cs
@@ -4,6 +4,15 @@ using System.Collections;
 public class PlayerQuests : MonoBehaviour {
 
     private Quests playerQuests = new Quests();
+    private long count = 0;
+
+    //Just here to quickly complete the quest. For testing
+    void Update() {
+        count++;
+        if (count > 300) {
+            CompleteQuest("Quest1");
+        }
+    }
 
     public bool IsQuestCompleted(string questName) {
         return playerQuests.IsQuestCompleted(questName);
