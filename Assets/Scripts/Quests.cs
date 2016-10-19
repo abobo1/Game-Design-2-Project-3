fea66cea5f53ad00d692ae95b8bfc33f4a63a24a
kanestapler <kanestapler@gmail.com>
2016-10-18 20:33:47 -0500

    Making quest giver respond to player

--- Assets/Scripts/Quests.cs
+++ Assets/Scripts/Quests.cs
@@ -7,31 +7,31 @@ public class Quests {
     private Hashtable allQuests = new Hashtable();
 
     public Quests() {
-        allQuests.Add("Quest1", new Quest("Test1", "This is quest #1", 150, false));
-        allQuests.Add("Quest2", new Quest("Test2", "This is quest #2", 300, false));
+        allQuests.Add("Quest1", new Quest("Quest1", "This is quest #1", 150, false));
+        allQuests.Add("Quest2", new Quest("Quest1", "This is quest #2", 300, false));
     }
 
-    public String GetQuestName(String questName) {
+    public string GetQuestName(string questName) {
         Quest x = allQuests[questName] as Quest;
         return x.name;
     }
 
-    public String GetQuestDescription(String questName) {
+    public string GetQuestDescription(string questName) {
         Quest x = allQuests[questName] as Quest;
         return x.description;
     }
 
-    public int GetQuestExperience(String questName) {
+    public int GetQuestExperience(string questName) {
         Quest x = allQuests[questName] as Quest;
         return x.experience;
     }
 
-    public bool IsQuestCompleted(String questName) {
+    public bool IsQuestCompleted(string questName) {
         Quest x = allQuests[questName] as Quest;
         return x.completed;
     }
 
-    public void CompleteQuest(String questName) {
+    public void CompleteQuest(string questName) {
         Quest x = allQuests[questName] as Quest;
         x.completed = true;
         allQuests[questName] = x;
@@ -39,13 +39,13 @@ public class Quests {
 
     private class Quest {
 
-        public String name;
-        public String description;
+        public string name;
+        public string description;
         public int experience;
         public bool completed;
        // public ArrayList itemsGiven;
 
-        public Quest(String name, String description, int experience, bool completed) {
+        public Quest(string name, string description, int experience, bool completed) {
             this.name = name;
             this.description = description;
             this.experience = experience;
