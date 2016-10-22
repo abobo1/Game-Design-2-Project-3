f50b80628c4738d5b03f127a595043846a4d7c5b
Steven Cortez <steven_cortez@msn.com>
2016-10-22 15:34:08 -0500

    made changes to select screen and ui

---
+++ Assets/Scripts/Menus/ingame Hud/playerHud.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class playerHud : MonoBehaviour {
+    public GameObject[] menus;
+	public void openMenu(int menuId)
+    {
+        closeAllMenu();
+        menus[menuId].SetActive(true);
+         
+            
+
+    }
+    public void closeAllMenu()
+    {
+        for (int k=0;k<menus.Length;k++)
+            menus[k].SetActive(false);
+            
+    }
+    public void closeMenu (int menuId)
+    {
+        menus[menuId].SetActive(false);
+    }
+}
