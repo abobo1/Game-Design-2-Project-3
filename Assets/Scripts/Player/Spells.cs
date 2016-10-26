4109951121f3a7bd07ae2fe2c1e3d160237763d2
Steven Cortez <steven_cortez@msn.com>
2016-10-26 10:52:54 -0500

    made changes to Bobbies colliders, added scripts for playerstat, spells, and items

---
+++ Assets/Scripts/Player/Spells.cs
@@ -0,0 +1,34 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+
+public class Spells : MonoBehaviour {
+
+    private List<List<spell>> spellList;
+
+    struct spell
+    {
+        public string spellName;
+        public float damage, cost, castTime;
+
+        
+
+    }
+    
+    void Start () {
+        spell spells;
+        List<spell> charSpells;
+        spells.spellName = "";
+        spells.damage = 5;
+        spells.cost = 5;
+        spells.castTime = 2;
+
+        //charSpells.Add(spells);
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
---
+++ Assets/Scripts/Player/playerStat.cs
@@ -0,0 +1,65 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+
+public class playerStat : MonoBehaviour {
+
+    private float hpCurrent, hpMax, mpCurrent, mpMax, intellect, spirit, stamina, strength;
+    private int playerId, playerLevel, skillPoints,skillPointMax;
+    private List<Items> playerInv;
+    private List<Spells> playerSpells;
+
+    public playerStat()
+    {
+        playerId = GameObject.Find("GameController").GetComponent<mainMenu>().playerId;
+        playerLevel = 1;
+        skillPoints = 0;
+        skillPointMax = 0;
+        spirit = 20;
+        stamina = 20;
+
+        if (playerId == 0 || playerId == 2)
+        { intellect = 20; strength = 5;}
+        else
+        { intellect = 5;strength = 20; }
+
+    }
+
+    void Start() {
+
+    }
+    public float getHpCurrent(){
+        return hpCurrent;
+    }
+    public float getHpMax(){
+        return hpMax;
+    }
+    public float getMpCurrent()
+    {
+        return mpCurrent;
+    }
+    public float getMpMax()
+    {
+        return mpMax;
+    }
+
+    public int getLevel()
+    {
+        return playerLevel;
+    }
+    public int getSkillPoints()
+    {
+        return skillPoints;
+    }
+    
+    void setHealthandMp()
+    {
+        hpCurrent = getHpMax();
+        mpCurrent = getMpMax();
+    }
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
