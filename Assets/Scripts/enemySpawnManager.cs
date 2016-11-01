4c6404e038aacbef48e82e33ab83783d5f0e2301
Ben <bhoskins@stu.jsu.edu>
2016-11-01 00:41:13 -0500

    Rough enemy spawn and prefabs for enemies

---
+++ Assets/Scripts/enemySpawnManager.cs
@@ -0,0 +1,32 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class enemySpawnManager : MonoBehaviour {
+
+    public GameObject enemy;
+    public int spawnAmount;
+    public int distance;
+    public float spawnTime; 
+    public Transform[] spawnPoints;
+    public Transform player;
+
+    // Use this for initialization
+    void Start()
+    {
+        InvokeRepeating("Spawn", spawnTime, spawnTime);
+    }
+
+    void Spawn()
+    {
+        if (Vector3.Distance(transform.position, player.position) < distance)
+        {
+            GameObject[] array;
+            array = GameObject.FindGameObjectsWithTag("Enemy");
+            if (spawnAmount > array.Length)
+            {
+                int spawnPointIndex = Random.Range(0, spawnPoints.Length);
+                Instantiate(enemy, spawnPoints[spawnPointIndex].position, spawnPoints[spawnPointIndex].rotation);
+            }
+        }
+    }
+}
