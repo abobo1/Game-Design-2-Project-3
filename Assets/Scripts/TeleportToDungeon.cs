359805a164ce5ef76fd2f24573fe9f10cdd3456d
kanestapler <kanestapler@gmail.com>
2016-11-08 13:31:42 -0600

    Teleport script

---
+++ Assets/Scripts/TeleportToDungeon.cs
@@ -0,0 +1,14 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.SceneManagement;
+
+public class TeleportToDungeon : MonoBehaviour {
+
+    public string levelToLoad = "dungeon";
+
+    void OnTriggerEnter(Collider other) {
+        if (other.tag == "Player") {
+            SceneManager.LoadScene(levelToLoad);
+        }
+    }
+}
