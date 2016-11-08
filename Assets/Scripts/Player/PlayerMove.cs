d8f4a2b8d16321904ed0461a751699444519a550
Ben <bhoskins@stu.jsu.edu>
2016-11-08 13:56:15 -0600

    movement changed, no rigid body

--- Assets/Scripts/Player/PlayerMove.cs
+++ Assets/Scripts/Player/PlayerMove.cs
@@ -4,6 +4,9 @@ using System.Collections;
 public class PlayerMove : MonoBehaviour {
 
 	public float moveSpeed, turningspeed;
+    public float gravity = 20.0f;
+    private Vector3 moveDirection = Vector3.zero;
+
 	void Start () {
 	
 	}
@@ -12,12 +15,34 @@ public class PlayerMove : MonoBehaviour {
 	void Update () {
         float vert = Input.GetAxis("Vertical");
         float horiz = Input.GetAxis("Horizontal");
-        
-        if (vert >0)
+        /*
+        if (vert > 0)
             transform.Translate(0, 0, vert * moveSpeed * Time.deltaTime);
         else if (vert < 0)
             transform.Translate(0, 0, vert * moveSpeed/3 * Time.deltaTime);
-        transform.Rotate(0, horiz * Time.deltaTime * turningspeed , 0);
-	
+            */
+        CharacterController controller = GetComponent<CharacterController>();
+
+        if(vert > 0)
+        {
+            moveDirection = new Vector3(horiz, 0, vert);
+            moveDirection = transform.TransformDirection(moveDirection);
+            moveDirection *= moveSpeed;
+            controller.Move(moveDirection * Time.deltaTime);
+            transform.Rotate(Vector3.up, 2 * horiz * Time.deltaTime * turningspeed);
+        }
+        else if(vert < 0)
+        {
+            moveDirection = new Vector3(horiz, 0, vert);
+            moveDirection = transform.TransformDirection(moveDirection);
+            moveDirection *= moveSpeed/3;
+            controller.Move(moveDirection * Time.deltaTime);
+            transform.Rotate(Vector3.up, 2 * horiz * Time.deltaTime * turningspeed);
+        }
+        else
+            transform.Rotate(Vector3.up, horiz * Time.deltaTime * turningspeed);
+
+        moveDirection.y -= gravity * Time.deltaTime;
+        Debug.Log(horiz * Time.deltaTime * turningspeed);
 	}
 }
---
+++ Assets/test.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class test : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+    void OnCollisionEnter(Collision collider)
+    {
+        Debug.Log("Collider works but not really");
+        Debug.Log(collider.gameObject.name);
+    }
+
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
