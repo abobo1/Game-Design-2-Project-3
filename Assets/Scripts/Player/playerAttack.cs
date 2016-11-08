487c0c208fe8a00333e4e4f1e8a5bfc060fa4144
Steven Cortez <steven_cortez@msn.com>
2016-11-08 08:18:28 -0600

    Did stuff on character spell Buttons

---
+++ Assets/Scripts/Player/playerAttack.cs
@@ -0,0 +1,47 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class playerAttack : MonoBehaviour {
+
+
+    bool toolTip = false;
+    string spellName;
+    float uiWidth, uiHeight;
+
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+    public void changeIcon(int id)
+    {
+        uiHeight = Screen.height - 100;
+        //change pos
+        if (id == 0)
+            uiWidth = Screen.width - 1025;
+        else if (id ==1)
+            uiWidth = Screen.width - 990;
+    }
+    public void Hover(string spellName)
+    {
+        //Debug.Log(spellName);
+        toolTip = true;
+        this.spellName = spellName;
+
+       
+    }
+    public void hideUI()
+    {
+        toolTip = false;
+    }
+
+    void OnGUI()
+    {
+        if (toolTip)
+        GUI.Label(new Rect(uiWidth,uiHeight , 128, 32), spellName);
+    }
+    }
