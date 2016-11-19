77b4b680948b20a5f7e8dc67ead883d921e2dc3b
Trey50Daniel <tagg8945@gmail.com>
2016-11-19 10:17:29 -0600

    Made Enemy look at player

--- Assets/Scripts/Enemy/Enemy.cs
+++ Assets/Scripts/Enemy/Enemy.cs
@@ -3,13 +3,53 @@ using System.Collections;
 
 public class Enemy : MonoBehaviour {
 
+	private Transform target;
+    public float speed = 2.5f;
+    public float attack1Range = 1f;
+    public int attack1Damage = 1;
+    public float timeBetweenAttacks;
+	private Animator enemyAni;
+	private HashID enemyHashID;
+	public float attackDistance;
+
+	
+
 	// Use this for initialization
 	void Start () {
-	
+		GameObject player = GameObject.FindGameObjectWithTag("Player");
+		target = player.transform;
+		enemyAni = GetComponent<Animator>();
+		enemyHashID = GetComponent<HashID>();
+		Rest ();
 	}
 	
 	// Update is called once per frame
 	void Update () {
-	
+		attackDistance = Vector3.Distance(transform.position, target.position);
+		if(attackDistance < 50.0f) {
+			MoveToPlayer();
+		}
+		else {
+			Rest();
+		}
 	}
+
+	public void MoveToPlayer ()
+    {
+        //rotate to look at player
+        transform.LookAt (target.position);
+        transform.Rotate (new Vector3 (0, -90, 0), Space.Self);
+		enemyAni.SetFloat(enemyHashID.speed, 3.0f);
+         
+        //move towards player
+        if (Vector3.Distance (transform.position, target.position) > attack1Range) 
+        {
+            transform.Translate (new Vector3 (speed * Time.deltaTime, 0, 0));
+        }
+    }
+ 
+    public void Rest ()
+    {
+		enemyAni.SetFloat(enemyHashID.speed, 0.0f);
+    }
 }
