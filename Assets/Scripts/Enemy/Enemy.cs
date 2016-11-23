9733447e5614989976af0db442e488308e9c2d93
Steven Cortez <steven_cortez@msn.com>
2016-11-23 09:25:53 -0600

    made changes to enemy targeting, movement, and animations

--- Assets/Scripts/Enemy/Enemy.cs
+++ Assets/Scripts/Enemy/Enemy.cs
@@ -4,52 +4,71 @@ using System.Collections;
 public class Enemy : MonoBehaviour {
 
 	private Transform target;
-    public float speed = 2.5f;
-    public float attack1Range = 1f;
-    public int attack1Damage = 1;
-    public float timeBetweenAttacks;
-	private Animator enemyAni;
-	private HashID enemyHashID;
-	public float attackDistance;
+    private float speed = 2.5f;
+    private float attack1Range = 10f;
+    private float attack2Range = 5f;
+    private float attackRange;
+    private int attack1Damage = 1;
+    private int attack2Damage = 1;
+    private float timeBetweenAttacks;
+    public Animator ani;
+    public HashID hash;
+    private float attackDistance;
+    private GameObject player;
+    public string enemyType;
+
 
 	
 
 	// Use this for initialization
 	void Start () {
-		GameObject player = GameObject.FindGameObjectWithTag("Player");
-		target = player.transform;
-		enemyAni = GetComponent<Animator>();
-		enemyHashID = GetComponent<HashID>();
-		Rest ();
+        
+        player = GameObject.FindGameObjectWithTag("Player");
+        target = GameObject.FindGameObjectWithTag("Player").transform;
+        ani = GetComponent<Animator>();
+        hash = GetComponent<HashID>();
+       // ani.SetFloat(hash.speed, 5f);
+        Rest ();
+
+        if (enemyType == "Close")
+            attackRange = attack2Range;
+        else if (enemyType == "Ranged")
+            attackRange = attack1Range;
+
 	}
 	
 	// Update is called once per frame
 	void Update () {
-		attackDistance = Vector3.Distance(transform.position, target.position);
+        //ani.SetFloat(hash.speed, 5f);
+        GetComponent<Animator>().SetFloat(GetComponent<HashID>().speed, 0);
+        attackDistance = Vector3.Distance(transform.position, target.position);
 		if(attackDistance < 50.0f) {
 			MoveToPlayer();
-		}
+        }
 		else {
-			Rest();
+			//Rest();
 		}
 	}
 
 	public void MoveToPlayer ()
     {
         //rotate to look at player
-        transform.LookAt (target.position);
-        transform.Rotate (new Vector3 (0, -90, 0), Space.Self);
-		enemyAni.SetFloat(enemyHashID.speed, 3.0f);
+        transform.LookAt (target);
+        //transform.Rotate (new Vector3 (0, -90, 0), Space.Self);
+		//enemyAni.SetFloat(enemyHashID.speed, 3.0f);
          
         //move towards player
-        if (Vector3.Distance (transform.position, target.position) > attack1Range) 
+        if (Vector3.Distance (transform.position, target.position) > attackRange) 
         {
-            transform.Translate (new Vector3 (speed * Time.deltaTime, 0, 0));
+            //transform.Translate (new Vector3 (speed * Time.deltaTime, 0, 0));
+            transform.Translate(Vector3.forward * Time.deltaTime * 5);
+            //enemyAni.SetFloat(enemyHashID.speed, 5);
         }
     }
  
     public void Rest ()
     {
-		enemyAni.SetFloat(enemyHashID.speed, 0.0f);
+        //ani.SetFloat(hash.speed, 0);
+        GetComponent<Animator>().SetFloat(GetComponent<HashID>().speed, 0);
     }
 }
---
+++ Assets/Scripts/Enemy/EnemyMove.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class EnemyMove : MonoBehaviour {
+
+    public GameObject player;
+	void Start () {
+        player = GameObject.FindGameObjectWithTag("Player");
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        transform.LookAt(player.transform);
+	
+	}
+}
